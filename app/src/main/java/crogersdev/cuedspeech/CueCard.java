f612944a55f62b04ed24f6263e5176bfcab5500d
Chris <creedrogers@gmail.com>
2015-10-25 22:13:44 -0400

    finally!  barebones working model.  :P

--- app/src/main/java/crogersdev/cuedspeech/CueCard.java
+++ app/src/main/java/crogersdev/cuedspeech/CueCard.java
@@ -2,9 +2,11 @@ package crogersdev.cuedspeech;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
@@ -16,7 +18,38 @@ public class CueCard extends Fragment {
         View rootView = inflater.inflate(R.layout.cuecard, container, false);
 
         Bundle args = getArguments();
-        ((TextView)rootView.findViewById(R.id.text)).setText("Page " + args.getInt("page_position"));
+        //((TextView)rootView.findViewById(R.id.text)).setText("Page " + args.getInt("page_position"));
+        ((TextView)rootView.findViewById(R.id.cuecard_label)).setText(args.getString("label"));
+        ((TextView)rootView.findViewById(R.id.cuecard_phonemes)).setText(args.getString("phonemes"));
+        ((TextView)rootView.findViewById(R.id.cuecard_mnemonic)).setText(args.getString("mnemonic"));
+        Log.d("crogersdev", "cue_image is: " + args.getInt("cue_image"));
+        switch (args.getInt("cue_image")) {
+            case 1:
+                rootView.findViewById(R.id.cue_image).setBackgroundResource(R.drawable.handshape_1);
+                break;
+            case 2:
+                rootView.findViewById(R.id.cue_image).setBackgroundResource(R.drawable.handshape_2);
+                break;
+            case 3:
+                rootView.findViewById(R.id.cue_image).setBackgroundResource(R.drawable.handshape_3);
+                break;
+            case 4:
+                rootView.findViewById(R.id.cue_image).setBackgroundResource(R.drawable.handshape_4);
+                break;
+            case 5:
+                rootView.findViewById(R.id.cue_image).setBackgroundResource(R.drawable.handshape_5);
+                break;
+            case 6:
+                rootView.findViewById(R.id.cue_image).setBackgroundResource(R.drawable.handshape_6);
+                break;
+            case 7:
+                rootView.findViewById(R.id.cue_image).setBackgroundResource(R.drawable.handshape_7);
+                break;
+            case 8:
+                rootView.findViewById(R.id.cue_image).setBackgroundResource(R.drawable.handshape_8);
+                break;
+        }
+
 
         return rootView;
     }

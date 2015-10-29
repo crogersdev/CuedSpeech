bf7b42776ded636be6e667238b99f574c280183a
Chris <creedrogers@gmail.com>
2015-10-28 22:48:31 -0400

    resetting xml layout to get the theme and color correct

--- app/src/main/java/crogersdev/cuedspeech/MainActivity.java
+++ app/src/main/java/crogersdev/cuedspeech/MainActivity.java
@@ -1,31 +1,11 @@
 package crogersdev.cuedspeech;
 
-import android.app.Activity;
-import android.content.Context;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
     CustomPagerAdapter mCustomPagerAdapter;
     ViewPager mViewPager;
 
@@ -33,6 +13,9 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
 
         mCustomPagerAdapter = new CustomPagerAdapter(getSupportFragmentManager(), this);
         mViewPager = (ViewPager) findViewById(R.id.pager);

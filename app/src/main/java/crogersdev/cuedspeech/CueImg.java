441a69bdd8b0f300f0f3bc20b34a97a8f600763d
Chris <creedrogers@gmail.com>
2015-12-29 20:48:45 -0500

    fixing some graphics

--- app/src/main/java/crogersdev/cuedspeech/CueImg.java
+++ app/src/main/java/crogersdev/cuedspeech/CueImg.java
@@ -15,14 +15,14 @@ public enum CueImg {
     HANDSHAPE6 (R.drawable.handshape_6_icon),
     HANDSHAPE7 (R.drawable.handshape_7_icon),
     HANDSHAPE8 (R.drawable.handshape_8_icon),
-    VOWEL_MOUTH (R.drawable.vowel_mouth),
-    VOWEL_CHIN (R.drawable.vowel_chin),
-    VOWEL_THROAT (R.drawable.vowel_throat),
-    VOWEL_SIDE (R.drawable.vowel_side),
-    VOWEL_SIDE_FWD (R.drawable.vowel_side_fwd),
-    VOWEL_SIDE_DOWN (R.drawable.vowel_side_down),
-    VOWEL_CHIN_THROAT (R.drawable.vowel_chin_throat),
-    VOWEL_SIDE_THROAT (R.drawable.vowel_side_throat);
+    VOWEL_MOUTH (R.drawable.vowel_mouth_icon),
+    VOWEL_CHIN (R.drawable.vowel_chin_icon),
+    VOWEL_THROAT (R.drawable.vowel_throat_icon),
+    VOWEL_SIDE (R.drawable.vowel_side_icon),
+    VOWEL_SIDE_FWD (R.drawable.vowel_side_fwd_icon),
+    VOWEL_SIDE_DOWN (R.drawable.vowel_side_down_icon),
+    VOWEL_CHIN_THROAT (R.drawable.vowel_chin_throat_icon),
+    VOWEL_SIDE_THROAT (R.drawable.vowel_side_throat_icon);
 
     int id;
     CueImg (int id)  { this.id = id; }
